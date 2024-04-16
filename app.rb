19a618550df3be7bf165d4e13feb48f349c82b58
simbiri <122113428+simbiri@users.noreply.github.com>
2024-04-16 14:54:18 +0000

    Done majority

--- app.rb
+++ app.rb
@@ -15,7 +15,25 @@ get("/") do
 end
 
 get("/:currency")do
+
   @cur_currency = params.fetch("currency")
-  cur_quotes_url = "#{base_url}live?access_key=#{access_key}"
-  
+  cur_quotes_url = "#{base_url}live?access_key=#{access_key}" +\
+  "&source=#{@cur_currency}"
+  quotes_resp = HTTP.get(cur_quotes_url)
+  @quotes = JSON.parse(quotes_resp)["quotes"]
+
+  erb(:quotes)  
+end
+
+get("/:cur_org/:cur_dest")do
+
+  @cur_org = params.fetch("cur_org")
+  @cur_dest = params.fetch("cur_dest")
+
+  convert_url = "#{base_url}convert?access_key=#{access_key}" +\
+  "&from=#{@cur_org}&to=#{@cur_dest}&amount=1"
+  convert_resp = HTTP.get(convert_url)
+  @result = JSON.parse(convert_resp)["result"]
+
+  erb(:conversion)  
 end
