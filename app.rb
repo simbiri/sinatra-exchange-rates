09870ca00dac0ad5e2253cc5674c310b47324fc4
simbiri <122113428+simbiri@users.noreply.github.com>
2024-04-16 14:19:08 +0000

    port problem

--- app.rb
+++ app.rb
@@ -1,9 +1,21 @@
 require "sinatra"
 require "sinatra/reloader"
+require  "http"
+
+base_url = "https://api.exchangerate.host/"
+access_key = ENV["EXCHANGE_RATE_KEY"]
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+
+  cur_pairs_url = "#{base_url}list?access_key=#{access_key}"
+  cur_resp = HTTP.get(cur_pairs_url)
+  @currencies = JSON.parse(cur_resp)["currencies"]
+
+  erb(:currency_pairs)
+end
+
+get("/:currency")do
+  @cur_currency = params.fetch("currency")
+  cur_quotes_url = "#{base_url}live?access_key=#{access_key}"
+  
 end
